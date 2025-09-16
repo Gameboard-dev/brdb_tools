c99eb6f936e8c55551aaae0c4f2ae697b16c4f29
Gameboard-dev <6smithadam@gmail.com>
2025-09-16 02:31:14 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use brdb::{
-    pending::BrPendingFs, schema::{
+    fs::BrFs, pending::BrPendingFs, schema::{
         BrdbSchema, 
         BrdbSchemaGlobalData
-    }, BrFsReader, BrReader, Brdb, ChunkIndex, Entity, EntityChunkIndexSoA, EntityChunkSoA, IntoReader
+    }, BrReader, Brdb, Entity, EntityChunkIndexSoA, EntityChunkSoA, IntoReader
 };
 
+// Import the derive macro for BrFsReader if it is in a proc-macro crate
+use brdb::BrFsReader;
+
 use std::{
-    sync::Arc,
     env,
+    mem,
     fs,
+    sync::Arc,
     path::PathBuf,
 };
 
@@ -20,15 +24,32 @@ fn world_path(filename: &str) -> PathBuf {
 }
 
 fn is_dynamic_grid(entity: &Entity) -> bool {
-    return entity.data.get_schema_struct().is_some_and(|s| s.0.as_ref() == "Entity_DynamicBrickGrid")
+    return entity.data.get_schema_struct()
+    .is_some_and(|s| s.0.as_ref() == "Entity_DynamicBrickGrid")
 }
+
+struct Pending {
+    entity_files: Vec<(String, BrPendingFs)>,
+    grid_files: Vec<(String, BrPendingFs)>
+}
+
+impl Default for Pending {
+    fn default() -> Self {
+        Self {
+            entity_files: Vec::new(),
+            grid_files: Vec::new(),
+        }
+    }
+}
+
 
 struct WorldProcessor {
     global_data: Arc<BrdbSchemaGlobalData>,
     entity_schema: Arc<BrdbSchema>,
     db: BrReader<Brdb>,
-    pending: Vec<(String, BrPendingFs)>,
+    pending: Pending,
 }
+
 
 impl WorldProcessor {
 
@@ -40,69 +61,65 @@ impl WorldProcessor {
             global_data: db.global_data()?,
             entity_schema: db.entities_schema()?,
             db,
-            pending: vec![]
+            pending: Pending::default(),
         })
     }
 
     fn duplicate_entities(&mut self) -> Result<(), Box<dyn std::error::Error>> {
 
-        let mut old_entity_soa: EntityChunkIndexSoA = self.db.entity_chunk_index_soa()?;
-        let mut grid_ids = vec![1];
-        
-        for chunk_index in old_entity_soa.chunk_3d_indices {
-            
+        let mut src_entities: EntityChunkIndexSoA = self.db.entity_chunk_index_soa()?;
+
+        let grids: BrFs = self.db.get_fs()?.cd("/World/0/Bricks/Grids")?;
+
+        for chunk_index in src_entities.chunk_3d_indices {
+
             let entities: Vec<Entity> = self.db.entity_chunk(chunk_index)?;
-            let mut new_entity_soa = EntityChunkSoA::default();
+            let mut dst_entities = EntityChunkSoA::default();
 
-            for mut entity in entities.into_iter() {
+            for entity in entities.into_iter() {
                 
+                // Use original indexes
                 let index: u32 = entity.id.unwrap() as u32;
-                
-                entity.frozen = true;
-                new_entity_soa.add_entity(&self.global_data, &entity, index);
+                println!("Old Index {}", index);
+                dst_entities.add_entity(&self.global_data, &entity, index);
 
+                src_entities.next_persistent_index += 1; // Ensure unique entity indexing
+                let new_index: u32 = src_entities.next_persistent_index;
+                println!("New Index {}", new_index);
+
+                // Duplicate lacks grid
                 let mut duplicate: Entity = entity.clone();
                 duplicate.location.x += 200f32;
-
-                new_entity_soa.add_entity(&self.global_data, &duplicate, old_entity_soa.next_persistent_index);
-                old_entity_soa.next_persistent_index += 1;
+                duplicate.id = Some(new_index as usize);
+                
+                dst_entities.add_entity(&self.global_data, &duplicate, new_index);
 
                 // Determine if the chunk is a dynamic brick grid
                 // https://github.com/brickadia-community/brdb/blob/attempt-remove-shadows/crates/brdb/examples/world_remove_shadows.rs#L20-L23
+
                 if is_dynamic_grid(&entity) {
-                    if let Some(id) = entity.id {
-                        grid_ids.push(id);
+                    if let Some(grid_id) = entity.id {
+                        let dir: BrPendingFs = grids.cd(grid_id.to_string())?.to_pending(&*self.db)?;
+
+                        self.pending.grid_files.push((grid_id.to_string(), dir.clone()));
+                        println!("Pushed Dynamic Grid {}", grid_id.to_string());
+                        
+                        self.pending.grid_files.push((new_index.to_string(), dir));
+                        println!("Pushed Dynamic Grid {}", new_index.to_string())
                     }
-                }
-            }
+                };
+            };
 
-            let bytes: Vec<u8> = new_entity_soa.to_bytes(&self.entity_schema)?;
 
-            self.pending.push((
+            // Pushes updated dst_entities with duplicates
+            let bytes: Vec<u8> = dst_entities.to_bytes(&self.entity_schema)?;
+
+            self.pending.entity_files.push((
                 format!("{chunk_index}.mps"),
                 BrPendingFs::File(Some(bytes)),
             ));
+
         }
-
-        let mut grids_files = vec![];
-
-        for grid in &grid_ids {
-            grids_files.push(grid.to_string());
-        }
-
-        let patch = BrPendingFs::Root(vec![(
-            "World".to_owned(),
-            BrPendingFs::Folder(Some(vec![(
-                "0".to_string(),
-                BrPendingFs::Folder(Some(vec![(
-                    "Bricks".to_string(),
-                    BrPendingFs::Folder(Some(vec![(
-                        "Grids".to_string(),
-                        BrPendingFs::Folder(Some(grids_files)),
-                    )])),
-                )])),
-            )])),
-        )]);
 
         Ok(())
     }
@@ -114,16 +131,19 @@ impl WorldProcessor {
             BrPendingFs::Folder(Some(vec![
                 ("0".to_string(),
                 BrPendingFs::Folder(Some(vec![
-                    ("Entities".to_string(),
-                    BrPendingFs::Folder(Some(vec![
+                    ("Bricks".to_string(), BrPendingFs::Folder(Some(vec![(
+                        "Grids".to_string(),
+                        BrPendingFs::Folder(Some(mem::take(&mut self.pending.grid_files))),
+                    )]))),
+                    ("Entities".to_string(), BrPendingFs::Folder(Some(vec![
                         ("Chunks".to_string(),
-                        // `pending` is moved and emptied
-                        BrPendingFs::Folder(Some(std::mem::take(&mut self.pending))),
+                        BrPendingFs::Folder(Some(mem::take(&mut self.pending.entity_files))),
                     )])),
                 )])),
             )])),
         )]);
     }
+
 
     fn save_as(&mut self, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
         
@@ -140,6 +160,13 @@ impl WorldProcessor {
         Ok(())
     }
 
+    #[allow(dead_code)]
+    fn debug(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let grids: BrFs = self.db.get_fs()?.cd("/World")?;
+        println!("[debug] {}", grids.render());
+        Ok(())
+    }
+
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -147,4 +174,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     processor.duplicate_entities()?;
     processor.save_as("Monastery_V5.brdb")?;
     Ok(())
+    
 }
